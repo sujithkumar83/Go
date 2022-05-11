a581ad3859778b21194e717e949e3699dc2bc20b
sujithkumar83 <sujith.kumar83@outlook.com>
2022-05-11 21:34:44 +0530

    added the chunky transfer project

--- Assignments/main.go
+++ Assignments/main.go
@@ -5,7 +5,7 @@ import (
 	"math"
 )
 
-type intVar []int8
+type intVar []int
 
 func main() {
 
@@ -17,7 +17,7 @@ func main() {
 
 func newIntVar() intVar {
 
-	y := []int8{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	y := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	return y
 }
